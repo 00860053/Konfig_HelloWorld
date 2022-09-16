983251a394e27b8fc08d048c070976853de740a7
00860053 <00860053@edu.campus02.at>
2022-09-16 19:07:28 +0200

    Initial Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Welcome to KONF 2022!");
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4"); // Strg+D
+
+
+    }
+}
